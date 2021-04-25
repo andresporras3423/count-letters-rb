82c54fbd2ca371371e3cccdfb9290a827c2fe799
oscarporras02 <oscarrussi@outlook.com>
2021-04-25 12:45:26 -0500

    add method for show top in questions controller

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -13,4 +13,23 @@ class QuestionController < ApplicationController
         nQuestions = Question.where("correct=#{@@status[params[:correct]]}").order("created_at").limit(10)
         render json: nQuestions.as_json(only: %i[letter created_at]), status: :ok
     end
+
+    def show_top
+        sql = """
+        with q1 as (select letter,
+            count(*) as total,
+            sum(case
+            when correct=#{@@status[params[:correct]]}
+            then 1
+            else 0
+            end) as corrects
+            from questions
+            group by letter)
+            select letter, cast(corrects*100.0/total as decimal(18,2)) as percentaje, total, corrects
+            from q1
+            order by percentaje desc, total desc limit 10;
+        """;
+        top_questions = ActiveRecord::Base.connection.execute(sql)
+        render json: top_questions, status: :ok
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   put '/config_game/update', to: 'config_game#update'
   post '/question/save', to: 'question#save'
   get '/question/show_recent', to: 'question#show_recent'
+  get '/question/show_top', to: 'question#show_top'
 end
